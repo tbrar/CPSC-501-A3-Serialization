22ba39e73898d6205239abea8e18068cd0059856
tbrar <tbrar1@gmail.com>
2019-11-11 20:28:18 -0700

    Allow user to select between sender and receiver andserialize basic class info

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,27 @@
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Sender to Receiver?");
+		String selection = input.nextLine();
+		if(selection.equals("Sender")) {
+			try {
+				Sender sender = new Sender();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else if(selection.equals("Receiver")) {
+			
+		}
+		else {
+			System.out.println("That is not a valid option!");
+		}
+		return;
 	}
 
 }
--- src/Serializer.java
+++ src/Serializer.java
@@ -1,4 +1,15 @@
+import org.jdom2.DocType;
+import org.jdom2.Document;
+import org.jdom2.Element;
 
 public class Serializer {
-
+	Element root;
+	DocType type;
+	Document doc;
+	
+	public Serializer() {
+		root =  new Element("serialized");
+		type = new DocType("Objects");
+		doc = new Document(root, type);
+	}
 }
